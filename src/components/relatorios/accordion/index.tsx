56260a37d8eb22241016414bb902d1376220b409
matheusstyt <matheus20galdino@gmail.com>
2023-05-30 17:38:17 -0400

    bug-fix: alguns errors warning ok

--- src/components/relatorios/accordion/index.tsx
+++ src/components/relatorios/accordion/index.tsx
@@ -12,34 +12,34 @@ export default function AccordionDinamic (props : AccordionnDinamic) {
     const [expanded, setExpanded] = useState<boolean>(true);
     
     return (
-        <>
+        <section>
             <h3 className="title-relatorio">{props.title}</h3>
-                <Accordion 
-                    sx={{width: "100%", margin: 0}}
-                    expanded={expanded}
-                    onChange={()=>{
-                        setExpanded(!expanded);
-                    }
-                }>
-                    <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2a-content"
-                    id="panel2a-header"
-                    >
-                    <Typography style={{
-                                    fontWeight: 'bold',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyItems: 'center',
-                                    gap: 16,
-                                    }}> <FiFilter size={25}/> Filtro</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                    <Typography>
-                        {props.component}
-                    </Typography>
-                    </AccordionDetails>
-                </Accordion>
-        </>
+            <Accordion 
+                sx={{width: "100%", margin: 0}}
+                expanded={expanded}
+                onChange={()=>{
+                    setExpanded(!expanded);
+                }
+            }>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+                >
+                <Typography style={{
+                                fontWeight: 'bold',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyItems: 'center',
+                                gap: 16,
+                                }}> <FiFilter size={25}/>Filtro</Typography>
+                </AccordionSummary>
+           
+                
+                {props.component}
+                
+              
+            </Accordion>
+        </section>
     )
 }
